133fa52846ed0c5a49c80907726e1f7b7fde3493
Amit Bhardwaj (IN98390) <amit.61098390@ltimindtree.com>
2024-10-15 11:16:31 +0530

    updated deployment yaml file and added loggers

--- src/main/java/com/casestudy/a61098390/awsWeatherApiCasestudy/controller/WeatherDetailController.java
+++ src/main/java/com/casestudy/a61098390/awsWeatherApiCasestudy/controller/WeatherDetailController.java
@@ -2,6 +2,8 @@ package com.casestudy.a61098390.awsWeatherApiCasestudy.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,22 +22,28 @@ import jakarta.validation.Valid;
 @RestController
 public class WeatherDetailController {
 
+	Logger logger = LoggerFactory.getLogger(WeatherDetailController.class);
+
 	@Autowired
 	private WeatherDetailService service;
 	
 	@GetMapping("/allWeather")
 	private List<WeatherDetailDto> getWeatherDetail() {
+		logger.info("WeatherDetailController:: Getting weather details...");
 		return service.getAllDetails();
 	}
 	
 	@GetMapping("/weather/{id}")
 	private ResponseDto getWeatherDetail(@PathVariable int id) {
+		logger.info("WeatherDetailController:: Getting weather detail for given city id:: " + id);
 		ResponseDto dto = service.getWeatherByCityId(id);
 		if (dto != null) {
+			logger.info("Weather details for city " + dto.getCityName() + " fetched successfully!!");
 			dto.setResponseCode(200);
 			dto.setResponseMessage("Weather details for city " + dto.getCityName() + " fetched successfully!!");
 			return dto;
 		} else {
+			logger.info("Weather details for city Id: " + dto.getCityId() + " not found!!");
 			dto = new ResponseDto();
 			dto.setResponseCode(404);
 			dto.setResponseMessage("Weather details for city Id: " + dto.getCityId() + " not found!!");
@@ -45,6 +53,7 @@ public class WeatherDetailController {
 	
 	@PostMapping("/add")
 	private ResponseDto addWeatherDetail(@RequestBody @Valid InputCityDto dto) {
+		logger.info("WeatherDetailController:: Adding weather details for city:: " + dto.getCity());
 		ResponseDto res = service.addWeatherDetails(dto);
 		if (res.getResponseMessage() == null) {
 			res.setResponseMessage("Weather details fetched successfully.");
@@ -54,13 +63,16 @@ public class WeatherDetailController {
 	
 	@DeleteMapping("/delete/{id}")
 	private ResponseDto deleteWatherDetails(@PathVariable int id) {
+		logger.info("WeatherDetailController:: Delete weather details for city:: " + id);
 		ResponseDto dto = new ResponseDto();
 		boolean res = service.deleteWeatherDetails(0);
 		if (res) {
+			logger.info("WeatherDetailController:: Weather details for city id: " + id + " deleted successfully!!");
 			dto.setResponseCode(200);
 			dto.setResponseMessage("Weather details for city id: " + id + " deleted successfully!!");
 			return dto;
 		} else {
+			logger.info("WeatherDetailController:: Weather details for city id: " + id + " not found!!");
 			dto.setResponseCode(404);
 			dto.setResponseMessage("Weather details for city id: " + id + " not found!!");
 			return dto;
--- src/main/java/com/casestudy/a61098390/awsWeatherApiCasestudy/service/WeatherDetailService.java
+++ src/main/java/com/casestudy/a61098390/awsWeatherApiCasestudy/service/WeatherDetailService.java
@@ -3,10 +3,13 @@ package com.casestudy.a61098390.awsWeatherApiCasestudy.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -21,78 +24,93 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class WeatherDetailService {
 
+	Logger logger = LoggerFactory.getLogger(WeatherDetailService.class);
+	
 	@Autowired
 	private DynamoDBMapper mapper;
-	
+
 	@Value("${open.weather.api}")
 	private String weatherApi;
-	
+
 	public List<WeatherDetailDto> getAllDetails() {
+		logger.info("WeatherDetailService:: Getting weather details...");
 		DynamoDBScanExpression scan = new DynamoDBScanExpression();
 		List<WeatherDetailEntity> results = mapper.scan(WeatherDetailEntity.class, scan);
 		return results.stream().map(this::toWeatherDto).collect(Collectors.toList());
 	}
-	
+
 	public ResponseDto getWeatherByCityId(int id) {
+		logger.info("WeatherDetailService:: Getting weather details...");
 		WeatherDetailEntity item = mapper.load(WeatherDetailEntity.class, id);
 		if (item != null) {
+			logger.info("WeatherDetailService:: Getting weather details...");
 			return toResponseDto(item);
 		}
+		logger.info("WeatherDetailService:: Getting weather details...");
 		return null;
 	}
-	
+
 	public ResponseDto addWeatherDetails(InputCityDto dto) {
-		
+		logger.info("WeatherDetailService:: Getting weather details...");
 		ObjectMapper objMapper = new ObjectMapper();
 		RestTemplate template = new RestTemplate();
 		
-		Object temp = template.getForObject(weatherApi+dto.getCity(), Object.class);
-		WeatherObjDto weatherObj = objMapper.convertValue(temp, WeatherObjDto.class);
-		
 		ResponseDto response = new ResponseDto();
-		
-		if (weatherObj.getCod() == 200) {
-			response.setCityId(weatherObj.getId());
-			response.setCityName(weatherObj.getName());
-			response.setWeatherDesc(weatherObj.getWeather().get(0).getDescription());
-			response.setLon(weatherObj.getCoord().getLon());
-			response.setLat(weatherObj.getCoord().getLat());
-			response.setMinTemp(weatherObj.getMain().getTemp_min());
-			response.setMaxTemp(weatherObj.getMain().getTemp_max());
-			response.setTemp(weatherObj.getMain().getTemp());
-			response.setHumidity(weatherObj.getMain().getHumidity());
-			response.setResponseCode(weatherObj.getCod());
-			
-			mapper.save(toEntity(response));
-		} else {
-			response.setResponseMessage(weatherObj.getMessage());
-			response.setResponseCode(weatherObj.getCod());
+
+		try {
+			Object temp = template.getForObject(weatherApi + dto.getCity(), Object.class);
+			WeatherObjDto weatherObj = objMapper.convertValue(temp, WeatherObjDto.class);
+
+
+			if (weatherObj.getCod() == 200) {
+				logger.info("WeatherDetailService:: Getting weather details...");
+				response.setCityId(weatherObj.getId());
+				response.setCityName(weatherObj.getName());
+				response.setWeatherDesc(weatherObj.getWeather().get(0).getDescription());
+				response.setLon(weatherObj.getCoord().getLon());
+				response.setLat(weatherObj.getCoord().getLat());
+				response.setMinTemp(weatherObj.getMain().getTemp_min());
+				response.setMaxTemp(weatherObj.getMain().getTemp_max());
+				response.setTemp(weatherObj.getMain().getTemp());
+				response.setHumidity(weatherObj.getMain().getHumidity());
+				response.setResponseCode(weatherObj.getCod());
+
+				mapper.save(toEntity(response));
+			} else {
+				logger.info("WeatherDetailService:: Getting weather details...");
+				response.setResponseMessage(weatherObj.getMessage());
+				response.setResponseCode(weatherObj.getCod());
+			}
+		} catch(RestClientException | IllegalArgumentException e) {
+			logger.error("WeatherDetailService:: Getting weather details..." + e);
 		}
 		return response;
 	}
-	
+
 	public boolean deleteWeatherDetails(int id) {
+		logger.info("WeatherDetailService:: Getting weather details...");
 		WeatherDetailEntity item = mapper.load(WeatherDetailEntity.class, id);
-		
+
 		if (item != null) {
-			System.out.println("Update:: Weather deetails found for city id: " + id);
+			logger.info("WeatherDetailService:: Weather deetails found for city id: " + id);
 			mapper.delete(item);
 			return true;
 		}
 		return false;
 	}
-	
+
 	private ResponseDto toResponseDto(WeatherDetailEntity entity) {
 		ResponseDto dto = new ResponseDto();
 		BeanUtils.copyProperties(entity, dto);
 		return dto;
 	}
+
 	private WeatherDetailDto toWeatherDto(WeatherDetailEntity entity) {
 		WeatherDetailDto dto = new WeatherDetailDto();
 		BeanUtils.copyProperties(entity, dto);
 		return dto;
 	}
-	
+
 	private WeatherDetailEntity toEntity(WeatherDetailDto dto) {
 		WeatherDetailEntity entity = new WeatherDetailEntity();
 		BeanUtils.copyProperties(dto, entity);
